cd56c5393e91800bb45cc0eb14dfa250bd3d5b3b
Rabbit <dawoodahmed696@gmail.com>
2019-10-23 05:49:23 +0500

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -12,7 +12,8 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  
 }
 </script>
 
@@ -25,4 +26,69 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+html {
+  position: relative;
+  top: 0;
+  left: 0;
+  max-width: 100vw;
+  min-height: 200vh;
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+}
+body {
+  padding: 1.7vmin;
+  font-size: 16px;
+  font-family: sans, sans-serif;
+  color: #333;
+}
+
+a {
+  color: inherit;
+  font-style: italic;
+}
+
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 3em;
+  width: 100%;
+  background-color: #EEE;
+  box-sizing: border-box;
+  padding: 12px 18px;
+  box-shadow: 0px 4px 7px #777;
+  transition: background-color 0.4s ease-out;
+}
+
+nav span {
+  font-weight: 600;
+  letter-spacing: .085em;
+}
+
+nav button {
+  position: absolute;
+  top: 0;
+  transform: translateY(50%);
+  right: 0;
+  min-height: 1.5em;
+  min-width: 80px;
+  background-color: #eee;
+  color: #222;
+  border: 0px;
+  border-radius: 2px;
+  margin-right: 18px;
+  text-transform: uppercase;
+}
+
+nav.scroll {
+  background-color: #A1572F;
+  color: #eef;
+}
+
+body > header {
+  margin-top: 4.7em;
+}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div id="op container">
+    <!-- :class="[backgroundss]" -->
+     <b-navbar toggleable="lg" type="dark" variant=""  :class="getClass"   fixed="top" v-on:scroll.passive='handleScroll'>
+            <div class="container">
+                <b-navbar-brand href="http://localhost:8080/#home" class="py-2 px-4">
+                    <!-- <img src="../../public/logo.png" width="56" alt=""> -->
+                    <span class="title">Bake'n'Rolls</span>
+                </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav class="left" style="float: left; text-align:left;color:#000;">
+                        <b-nav-item class="active" href="http://localhost:8080/#home">Home</b-nav-item>
+                        <b-nav-item href="http://localhost:8080/#features">Features</b-nav-item>
+                        <b-nav-item href="http://localhost:8080/#dashboard">Dashboard</b-nav-item>
+                        <b-nav-item href="http://localhost:8080/#delegate">How To Delegate</b-nav-item>
+                        <b-nav-item href="http://localhost:8080/#faq">FAQ</b-nav-item>
+                        <b-nav-item href="http://localhost:8080/#contact">Contact Us</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </div>    
+        </b-navbar>
+
+<p style="margin-top:600px;">{{scrolls}}</p>
+  
+  </div> 
 </template>
 
 <script>
@@ -35,24 +33,91 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return{
+      backgroundss:'white',
+      scrolls:0
+    }
+  },
+  computed:{
+    getClass: function() {
+            
+            return this.scrolls > 100 ? 'transparent' : 'white';
+    }
+  },
+  methods: {
+    handleScroll (event) {
+      console.log("coming boiiii")
+      console.log(window.scrollY)
+      this.scrolls=window.scrollY
+    }
+  },
+  created () {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll);
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+
+<style>
+#op{
+  
+  background-color:grey;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#nav-collapse {
+    
+    margin-left: 40px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+a.nav-link {
+    font-size: 16px;
+    
+    margin-left: 40px;
+    color: gray !important;
 }
-a {
-  color: #42b983;
+
+a.nav-link:hover {
+    color: #7AC8ED !important;
 }
+
+a.nav-link:active {
+    color: #7AC8ED !important;
+}
+
+.navbar.navbar--hidden {
+  box-shadow: none;
+  transform: translate3d(0, -100%, 0);
+}
+
+.navbar {
+    
+    width: 99vw !important;
+}
+.navbar .navbar-brand {
+    color: #7AC8ED !important;
+    font-weight: 700;
+    font-size: 1.5rem;
+    padding-left: 20px;
+}
+.navbar.navbar-dark .navbar-brand div {
+    /* background-image: url('../../public/logo.png'); */
+}
+.title {
+    margin-left: 10px;
+}
+
+.white {
+    background-color: #fff !important;
+}
+
+.transparent {
+    background-color: black !important;
+}
+
 </style>
+
+
